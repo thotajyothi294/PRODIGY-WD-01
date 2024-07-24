83ad1361b9a75b6fb0bf95d82a6ae57935d2f2a1
thotajyothi294 <jyothithota971@gmail.com>
2024-07-24 17:11:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// JavaScript for adding scrolled class to header on scroll
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('header');
+    var scrollPosition = window.scrollY;
+  
+    if (scrollPosition > 50) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
+  
